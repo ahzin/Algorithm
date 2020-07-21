41a32deee4c85e2722bfdf48b3f622ab88abd76e
AHZIN <ahzin0605@gmail.com>
2020-07-21 18:16:29 +0900

    -

---
+++ Baekjoon/baekjoon_1001.java
@@ -0,0 +1,21 @@
+/*
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+첫째 줄에 A-B를 출력한다.
+*/
+import java.util.Scanner;
+
+
+class baekjoon_1001 { 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt(); 
+        int B = input.nextInt(); 
+
+        if ( 0 < A || B < 10) {
+        System.out.println(A - B);
+        }
+        else
+        return;
+    }
+}
